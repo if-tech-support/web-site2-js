c598ef291bd944b15a2b49b9a7df4e9501be5e18
takumi <akaranki4@gmail.com>
2021-09-05 15:20:57 +0900

    ハンバーガーメニュー実装 追加

---
+++ humberger/js/main.js
@@ -0,0 +1,32 @@
+{
+  /*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+
+  // 変数宣言
+  // ハンバーガーメニューの要素
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  // ハンバーガーメニューがクリックされた時に表示されるメニュー画面の要素
+  const navi = document.getElementById("hamburger-navigation");
+
+  // ハンバーガーメニュー内の各セクションの要素
+  const humbergerMenuSections = document.querySelectorAll(".humberger-menu-section");
+
+  // ハンバーガーメニューをクリックした時の処理
+  hamburgerMenu.addEventListener("click", function () {
+    // toggleを使用することで、hamburgerクラスとhamburger-navigationIDに
+    // activeクラスが存在する場合は削除、存在しない場合を追加する処理を自動で行ってくれる
+    hamburgerMenu.classList.toggle("active");
+    navi.classList.toggle('active');
+  });
+
+  // ハンバーガーメニュー内の各セクションをクリックした際、開いていたハンバーガーメニューを閉じる
+  humbergerMenuSections.forEach((humbergerMenuSection) => {
+    console.log(humbergerMenuSection);
+    humbergerMenuSection.addEventListener("click", function () {
+      hamburgerMenu.classList.remove("active");
+      navi.classList.remove("active");
+    });
+  });
+}
